a3d13fc836ecc3ffe9d26b4e859f170168660186
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-02 13:13:04 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let velocidad = 20
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            maqueen.motorStop(maqueen.Motors.M2)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorStop(maqueen.Motors.M1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+        }
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
